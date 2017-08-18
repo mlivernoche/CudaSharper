f4aacf8f9c09d0d30c1a6a42c9931470c981935e
Matthew Livernoche <mattalivernoche@gmail.com>
2017-08-18 03:17:17 -0700

    Update SplitArrays.cu

--- CudaSharperLibrary/SplitArrays.cu
+++ CudaSharperLibrary/SplitArrays.cu
@@ -2,7 +2,7 @@
 #include "device_launch_parameters.h"
 #include "cuda_profiler_api.h"
 
-__global__ void copyArrays(int *result, int *input, const unsigned int offset, const unsigned int length)
+__global__ void copyArrays(int *result, int *input, unsigned int offset, unsigned int length)
 {
 	int xid = (blockIdx.x * blockDim.x) + threadIdx.x;
 	if (xid < length) {
@@ -10,7 +10,7 @@ __global__ void copyArrays(int *result, int *input, const unsigned int offset, c
 	}
 }
 
-__global__ void copyArrays(long *result, long *input, const unsigned int offset, const unsigned int length)
+__global__ void copyArrays(long *result, long *input, unsigned int offset, unsigned int length)
 {
 	int xid = (blockIdx.x * blockDim.x) + threadIdx.x;
 	if (xid < length) {
@@ -18,7 +18,7 @@ __global__ void copyArrays(long *result, long *input, const unsigned int offset,
 	}
 }
 
-__global__ void copyArrays(float *result, float *input, const unsigned int offset, const unsigned int length)
+__global__ void copyArrays(float *result, float *input, unsigned int offset, unsigned int length)
 {
 	int xid = (blockIdx.x * blockDim.x) + threadIdx.x;
 	if (xid < length) {
@@ -26,7 +26,7 @@ __global__ void copyArrays(float *result, float *input, const unsigned int offse
 	}
 }
 
-__global__ void copyArrays(double *result, double *input, const unsigned int offset, const unsigned int length)
+__global__ void copyArrays(double *result, double *input, unsigned int offset, unsigned int length)
 {
 	int xid = (blockIdx.x * blockDim.x) + threadIdx.x;
 	if (xid < length) {
@@ -52,53 +52,33 @@ template<typename T> void splitArray(int device_id, T *src, T *array1, T *array2
 	cudaMalloc(&d_array1, array1_length * sizeof(T));
 	cudaMalloc(&d_array2, array2_length * sizeof(T));
 
-	cudaStream_t stream1;
-	cudaStreamCreate(&stream1);
+	cudaMemcpy(d_src, src, array_length * sizeof(T), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_array1, array1, array1_length * sizeof(T), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_array2, array2, array2_length * sizeof(T), cudaMemcpyHostToDevice);
 
-	cudaMemcpyAsync(d_src, src, array_length * sizeof(T), cudaMemcpyHostToDevice, stream1);
-	cudaMemcpyAsync(d_array1, array1, array1_length * sizeof(T), cudaMemcpyHostToDevice, stream1);
-	cudaMemcpyAsync(d_array2, array2, array2_length * sizeof(T), cudaMemcpyHostToDevice, stream1);
+	copyArrays << <blocks, threadsPerBlock, sizeof(int) >> > (d_array1, d_src, 0, array1_length);
+	copyArrays << <blocks, threadsPerBlock, sizeof(int) >> > (d_array2, d_src, split_index, array_length);
 
-	cudaStreamSynchronize(stream1);
-
-	cudaStream_t stream2;
-	cudaStreamCreate(&stream2);
-
-	copyArrays << <blocks, threadsPerBlock, 0, stream1 >> > (d_array1, d_src, 0, array1_length);
-	copyArrays << <blocks, threadsPerBlock, 0, stream2 >> > (d_array2, d_src, split_index, array2_length);
-
-	cudaStreamSynchronize(stream1);
-	cudaStreamSynchronize(stream2);
-
-	cudaMemcpyAsync(array1, d_array1, array1_length * sizeof(T), cudaMemcpyDeviceToHost, stream1);
-	cudaMemcpyAsync(array2, d_array2, array2_length * sizeof(T), cudaMemcpyDeviceToHost, stream1);
-
-	cudaStreamSynchronize(stream1);
-	cudaStreamDestroy(stream1);
-	cudaStreamDestroy(stream2);
+	cudaMemcpy(array1, d_array1, array1_length * sizeof(T), cudaMemcpyDeviceToHost);
+	cudaMemcpy(array2, d_array2, array2_length * sizeof(T), cudaMemcpyDeviceToHost);
 
 	cudaFree(d_src);
 	cudaFree(d_array1);
 	cudaFree(d_array2);
 }
 
-extern "C" __declspec(dllexport) void SplitIntArray(int device_id, int *src, int *array1, int *array2, const unsigned int array_length, const unsigned int split_index);
-extern "C" __declspec(dllexport) void SplitLongArray(int device_id, long *src, long *array1, long *array2, const unsigned int array_length, const unsigned int split_index);
-extern "C" __declspec(dllexport) void SplitFloatArray(int device_id, float *src, float *array1, float *array2, const unsigned int array_length, const unsigned int split_index);
-extern "C" __declspec(dllexport) void SplitDoubleArray(int device_id, double *src, double *array1, double *array2, const unsigned int array_length, const unsigned int split_index);
-
-void SplitIntArray(int device_id, int *src, int *array1, int *array2, const unsigned int array_length, const unsigned int split_index) {
+extern "C" __declspec(dllexport) void SplitIntArray(int device_id, int *src, int *array1, int *array2, const unsigned int array_length, const unsigned int split_index) {
 	splitArray<int>(device_id, src, array1, array2, array_length, split_index);
 }
 
-void SplitLongArray(int device_id, long *src, long *array1, long *array2, const unsigned int array_length, const unsigned int split_index) {
+extern "C" __declspec(dllexport) void SplitLongArray(int device_id, long *src, long *array1, long *array2, const unsigned int array_length, const unsigned int split_index) {
 	splitArray<long>(device_id, src, array1, array2, array_length, split_index);
 }
 
-void SplitFloatArray(int device_id, float *src, float *array1, float *array2, const unsigned int array_length, const unsigned int split_index) {
+extern "C" __declspec(dllexport) void SplitFloatArray(int device_id, float *src, float *array1, float *array2, const unsigned int array_length, const unsigned int split_index) {
 	splitArray<float>(device_id, src, array1, array2, array_length, split_index);
 }
 
-void SplitDoubleArray(int device_id, double *src, double *array1, double *array2, const unsigned int array_length, const unsigned int split_index) {
+extern "C" __declspec(dllexport) void SplitDoubleArray(int device_id, double *src, double *array1, double *array2, const unsigned int array_length, const unsigned int split_index) {
 	splitArray<double>(device_id, src, array1, array2, array_length, split_index);
 }
