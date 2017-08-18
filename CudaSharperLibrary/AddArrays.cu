556dbf870fad619d0f3367edbcd8dcbc2785f073
Matthew Livernoche <mattalivernoche@gmail.com>
2017-08-18 03:21:28 -0700

    Update AddArrays.cu

--- CudaSharperLibrary/AddArrays.cu
+++ CudaSharperLibrary/AddArrays.cu
@@ -8,30 +8,30 @@
  Functions for adding two arrays together. Requires CUDA 8.0.
 */
 
-__global__ void addArraysKernel(int *result, int *a, int *b, int count) {
+__global__ void addArraysKernel(int *result, int *a, int *b, unsigned int array_count) {
 	int xid = (blockIdx.x * blockDim.x) + threadIdx.x;
-	if (xid < count) {
+	if (xid < array_count) {
 		result[xid] = a[xid] + b[xid];
 	}
 }
 
-__global__ void addArraysKernel(float *result, float *a, float *b, int count) {
+__global__ void addArraysKernel(float *result, float *a, float *b, unsigned int array_count) {
 	int xid = (blockIdx.x * blockDim.x) + threadIdx.x;
-	if (xid < count) {
+	if (xid < array_count) {
 		result[xid] = a[xid] + b[xid];
 	}
 }
 
-__global__ void addArraysKernel(double *result, double *a, double *b, int count) {
+__global__ void addArraysKernel(double *result, double *a, double *b, unsigned int array_count) {
 	int xid = (blockIdx.x * blockDim.x) + threadIdx.x;
-	if (xid < count) {
+	if (xid < array_count) {
 		result[xid] = a[xid] + b[xid];
 	}
 }
 
-__global__ void addArraysKernel(long *result, long *a, long *b, int count) {
+__global__ void addArraysKernel(long *result, long *a, long *b, unsigned int array_count) {
 	int xid = (blockIdx.x * blockDim.x) + threadIdx.x;
-	if (xid < count) {
+	if (xid < array_count) {
 		result[xid] = a[xid] + b[xid];
 	}
 }
@@ -51,43 +51,32 @@ template<typename T> void addArrays(int device_id, T *result, T *array1, T *arra
 	cudaMalloc(&d_b, sizeof(T) * full_idx);
 	cudaMalloc(&dResult, sizeof(T) * full_idx);
 
-	cudaStream_t stream1;
-	cudaStreamCreate(&stream1);
-
-	cudaMemcpyAsync(d_a, array1, sizeof(T) * full_idx, cudaMemcpyHostToDevice, stream1);
-	cudaMemcpyAsync(d_b, array2, sizeof(T) * full_idx, cudaMemcpyHostToDevice, stream1);
-	cudaMemcpyAsync(dResult, result, sizeof(T) * full_idx, cudaMemcpyHostToDevice, stream1);
-
-	cudaStreamSynchronize(stream1);
+	cudaMemcpy(d_a, array1, sizeof(T) * full_idx, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_b, array2, sizeof(T) * full_idx, cudaMemcpyHostToDevice);
+	cudaMemcpy(dResult, result, sizeof(T) * full_idx, cudaMemcpyHostToDevice);
 
 	addArraysKernel << <blocks, threadsPerBlock >> > (dResult, d_a, d_b, full_idx);
 
 	cudaMemcpy(result, dResult, sizeof(T) * full_idx, cudaMemcpyDeviceToHost);
 
-	cudaStreamDestroy(stream1);
 	cudaFree(d_a);
 	cudaFree(d_b);
 	cudaFree(dResult);
 }
 
-extern "C" __declspec(dllexport) void AddIntArrays(int device_id, int *result, int *array1, int *array2, const int full_idx);
-extern "C" __declspec(dllexport) void AddFloatArrays(int device_id, float *result, float *array1, float *array2, const int full_idx);
-extern "C" __declspec(dllexport) void AddLongArrays(int device_id, long *result, long *array1, long *array2, const int full_idx);
-extern "C" __declspec(dllexport) void AddDoubleArrays(int device_id, double *result, double *array1, double *array2, const int full_idx);
-
-void AddIntArrays(int device_id, int *result, int *array1, int *array2, const int full_idx) {
+extern "C" __declspec(dllexport) void AddIntArrays(int device_id, int *result, int *array1, int *array2, const int full_idx) {
 	addArrays<int>(device_id, result, array1, array2, full_idx);
 }
 
-void AddFloatArrays(int device_id, float *result, float *array1, float *array2, const int full_idx) {
+extern "C" __declspec(dllexport) void AddFloatArrays(int device_id, float *result, float *array1, float *array2, const int full_idx) {
 	addArrays<float>(device_id, result, array1, array2, full_idx);
 }
 
-void AddLongArrays(int device_id, long *result, long *array1, long *array2, const int full_idx) {
+extern "C" __declspec(dllexport) void AddLongArrays(int device_id, long *result, long *array1, long *array2, const int full_idx) {
 	addArrays<long>(device_id, result, array1, array2, full_idx);
 }
 
-void AddDoubleArrays(int device_id, double *result, double *array1, double *array2, const int full_idx) {
+extern "C" __declspec(dllexport) void AddDoubleArrays(int device_id, double *result, double *array1, double *array2, const int full_idx) {
 	addArrays<double>(device_id, result, array1, array2, full_idx);
 }
 
