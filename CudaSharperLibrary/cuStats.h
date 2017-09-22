c27c36deede7db31df34ddc11dc423ff5047bade
Matthew Livernoche <mattalivernoche@gmail.com>
2017-09-22 14:58:25 -0700

    Added standard deviation (vs sample)

--- CudaSharperLibrary/cuStats.h
+++ CudaSharperLibrary/cuStats.h
@@ -15,5 +15,14 @@ template<typename T> double cuStats_standard_deviation(unsigned int device_id, T
 // Why, then, do these have to be marked as C? C++ will mangle the function names to support overloading.
 // Marking them as C will make sure that these function names will not be changed.
 // C... simply designed, simply frustrating.
+extern "C" __declspec(dllexport) double StandardDeviationFloat(unsigned int device_id, float *population, unsigned long long int population_size, double mean);
+extern "C" __declspec(dllexport) double StandardDeviationDouble(unsigned int device_id, double *population, unsigned long long int population_size, double mean);
+
+template<typename T> double cuStats_sample_standard_deviation(unsigned int device_id, T *sample, unsigned long long int sample_size, double mean);
+
+// C does not "support" function overloading like C++ does.
+// Why, then, do these have to be marked as C? C++ will mangle the function names to support overloading.
+// Marking them as C will make sure that these function names will not be changed.
+// C... simply designed, simply frustrating.
 extern "C" __declspec(dllexport) double SampleStandardDeviationFloat(unsigned int device_id, float *sample, unsigned long long int sample_size, double mean);
 extern "C" __declspec(dllexport) double SampleStandardDeviationDouble(unsigned int device_id, double *sample, unsigned long long int sample_size, double mean);
