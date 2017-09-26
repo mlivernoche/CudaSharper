b9b0b31363bf6e38b677630cda5dd35a1d8390a8
Matthew Livernoche <mattalivernoche@gmail.com>
2017-09-26 00:33:31 -0700

    Fixed a critical memory access bug...
    Added to unsigned long long int data_set_size to the list of parameters for the kernels. Before this, they were throwing a memory access violation bug, because the kernels weren't checking if they were accessing memory that was out of their scope. Also, made both kernels take mean as a double.

--- CudaSharperLibrary/cuStats.h
+++ CudaSharperLibrary/cuStats.h
@@ -7,8 +7,8 @@
 
 void cuArray_determine_launch_parameters(unsigned long int* blocks, unsigned long int* threads, unsigned long int* number_per_thread, unsigned long int max_block_size, unsigned long int max_thread_size);
 
-__global__ void cuStats_standard_deviation_kernel(float *std, float *sample, unsigned long int number_per_thread, float mean);
-__global__ void cuStats_standard_deviation_kernel(double *std, double *sample, unsigned long int number_per_thread, double mean);
+__global__ void cuStats_standard_deviation_kernel(float *std, float *sample, unsigned long int number_per_thread, unsigned long long int data_set_size, double mean);
+__global__ void cuStats_standard_deviation_kernel(double *std, double *sample, unsigned long int number_per_thread, unsigned long long int data_set_size, double mean);
 template<typename T> double cuStats_standard_deviation(unsigned int device_id, T *sample, unsigned long long int sample_size, double mean);
 
 // C does not "support" function overloading like C++ does.
